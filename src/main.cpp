2d732bd9fdcae027e683335be9c93d792ea85d8a
CarryLove <54007979+lkapitman@users.noreply.github.com>
2021-12-05 16:55:07 +0300

    сделал работу с датчиком hc-sr04

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,55 @@
 #include <Arduino.h>
 
-const int _pin = 13;
-const int _delay = 500;
+#define PIN_TRIG 12
+#define PIN_ECHO 11
+
+long duration, cm;
+
+int pin = 13;
+int delayTime = 500;
 
 void setup() {
-  pinMode(_pin, OUTPUT);
+
+  // Инициализируем взаимодействие по последовательному порту
+
+  Serial.begin (9600);
+  //Определяем вводы и выводы
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+  pinMode(pin, OUTPUT);
 }
 
-void blink(int _pin, int _delay) {
-  digitalWrite(_pin, HIGH);   // Включаем светодиод в высоком напряжении
-  delay(_delay);              // ждём пол-секунды
-  digitalWrite(_pin, LOW);    // Отключаем светодиот
-  delay(_delay);              // ждём пол-секунды
-
+void blink(int pin, int delayTime) {
+  digitalWrite(pin, HIGH);   // Включаем светодиод в высоком напряжении
+  delay(delayTime);              // ждём пол-секунды
+  digitalWrite(pin, LOW);    // Отключаем светодиот
+  delay(delayTime);              // ждём пол-секунды
 }
 
 void loop() {
-  blink(_pin, _delay);
+
+  blink(pin, delayTime);
+
+  // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
+
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);
+
+  // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы. 
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+
+  //  Время задержки акустического сигнала на эхолокаторе.
+  duration = pulseIn(PIN_ECHO, HIGH);
+
+  // Теперь осталось преобразовать время в расстояние
+  cm = (duration / 2) / 29.1;
+
+  Serial.print("Расстояние до объекта: ");
+  Serial.print(cm);
+  Serial.println(" см.");
+
+  // Задержка между измерениями для корректной работы скеча
+  delay(250);
 }
